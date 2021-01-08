d6a1864823a8cfed88bcc51fd4c739ce0c7954eb
Beardocracy <travisjbearden@gmail.com>
2021-01-07 18:53:37 -0600

    Added task

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This module contains minOperations
+"""
+
+def minOperations(n):
+    ''' Returns the sum of prime factors of a number'''
+    if n <= 0:
+        return 0
+    total = 0
+    divider = 2
+    while (n > 1):
+        if (n % divider == 0):
+            total = total + divider
+            n = n / divider
+            divider = 2
+        else:
+            if (divider == 2):
+                divider += 1
+            else:
+                divider += 2
+    return total
