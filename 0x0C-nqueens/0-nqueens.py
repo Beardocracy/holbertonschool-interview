85492e331ad34c15f7f410c93197e115ea6c6646
Beardocracy <travisjbearden@gmail.com>
2021-03-18 14:40:38 -0500

    Removed extra newlines

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -107,11 +107,11 @@ n = sys.argv[1]
 try:
     n = int(n)
 except ValueError:
-    print("N must be a number\n")
+    print("N must be a number")
     exit(1)
 
 if n < 4:
-    print("N must be at least 4\n")
+    print("N must be at least 4")
     exit(1)
 
 create_boards(n)
