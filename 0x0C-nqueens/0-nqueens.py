7c17f3d91990f419edd65a5290292511e1feee5a
Beardocracy <travisjbearden@gmail.com>
2021-03-18 14:55:01 -0500

    Changed exit to sys.exit

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -88,20 +88,23 @@ def create_boards(n):
 
 
 def main():
+    '''
+    Entry point for program
+    '''
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
-        exit(1)
+        sys.exit(1)
 
     n = sys.argv[1]
     try:
         n = int(n)
     except ValueError:
         print("N must be a number")
-        exit(1)
+        sys.exit(1)
 
     if n < 4:
         print("N must be at least 4")
-        exit(1)
+        sys.exit(1)
 
     create_boards(n)
 
