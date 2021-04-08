533b205b539646572f123b961260cb3495ae07bf
Beardocracy <travisjbearden@gmail.com>
2021-04-08 17:29:04 -0500

    Added part 0

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -2,12 +2,24 @@
 
 const request = require('request');
 const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`;
+
 request(url, (error, response, body) => {
     if (error) {
-        return
+        return;
     }
     const episode = JSON.parse(body);
-    console.log(episode);
-    console.log(response);
-
+    getCharacters(episode.characters, 0);
 });
+
+const getCharacters = (characters, index) => {
+    if (characters.length === index) {
+        return;
+    }
+    request(characters[index], (error, response, body) => {
+        if (error) {
+            return;
+        }
+        console.log(JSON.parse(body).name);
+        getCharacters(characters, ++index);
+    });
+};
