1af66e0b208d0d9323dbbba9d3cd73bf222d2b3b
Beardocracy <travisjbearden@gmail.com>
2021-04-15 12:18:26 -0500

    Changed logic for trickier configurations

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -6,11 +6,27 @@ def rain(walls):
     ''' Given a list of non-negative integers representing walls of width 1,
     calculate how much water will be retained after it rains.
     '''
+    size = len(walls) - 1
+    left_wall = walls[0]
+    prev_index = 0
     bucket = 0
-    for i in range(len(walls)):
-        if i != 0 and walls[i] == 0:
-            if walls[i - 1] < walls[i + 1]:
-                bucket += walls[i - 1]
+
+    temp = 0
+    for i in range(1, size + 1):
+        if (walls[i] >= left_wall):
+            left_wall = walls[i]
+            prev_index = i
+            temp = 0
+        else:
+            bucket += left_wall - walls[i]
+            temp += left_wall - walls[i]
+    if (prev_index < size):
+        bucket -= temp
+        left_wall = walls[size]
+        for i in range(size, prev_index - 1, -1):
+            if (walls[i] >= left_wall):
+                left_wall = walls[i]
             else:
-                bucket += walls[i + 1]
+                bucket += left_wall - walls[i]
+
     return (bucket)
