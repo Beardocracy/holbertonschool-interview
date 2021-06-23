e6cff330232a19fb4f5db96d96c260862fa4eac9
Beardocracy <travisjbearden@gmail.com>
2021-06-22 20:11:04 -0500

    Added part 0

---
+++ 0x19-making_change/0-making_change.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+''' Determine the fewest number of coins needed to meet a given total '''
+
+def makeChange(coins, total):
+    ''' Find the least number of coins needed '''
+    if (total <= 0):
+        return (0)
+
+    coins.sort(reverse=True)
+    coins_total = 0
+
+    for coin_value in coins:
+        times_coin_goes_into_total = total // coin_value
+        if (times_coin_goes_into_total > 0):
+            coins_total += times_coin_goes_into_total
+            total = total % coin_value
+
+    if (total == 0):
+        return (coins_total)
+    return (-1)
